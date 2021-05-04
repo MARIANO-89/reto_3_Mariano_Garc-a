cfc42d1a89575f7df04fcedc5f88524a7cd3e03f
MARIANO-89 <nano_carp@hotmail.com>
2021-05-04 19:20:15 -0300

    task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import List from './component/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [lists, setLists] = useState([]);
+    const [title, setTitle] = useState('');
+
+    const addList = () => {
+        let newList = [...lists];
+        newList.push({ title, List });
+
+        setLists(newList);
+    };
+    const deleteList = (index) => {
+        let deleteList = [...lists];
+        setLists(
+            deleteList.filter(function (lists, i) {
+                return i !== index;
+            })
+        );
+    };
+
+    return (
+        <div>
+            <div className='geeks'>
+                <h1 className='h1'>Reto 3 </h1>
+                <h1 className='h1__m'>
+                    <span> Geekshub Academy </span>
+                </h1>
+            </div>
+
+            <div className='input'>
+                <input
+                    placeholder='Introduce el título'
+                    className='Title'
+                    value={title}
+                    onChange={function (event) {
+                        setTitle(event.target.value);
+                    }}
+                ></input>
+                <button className='btn btn-info' onClick={addList}>
+                    {' '}
+                    Agregar Lista
+                </button>
+            </div>
+            <div className='lists'>
+                {lists.map(({ title, List }, index) => {
+                    return (
+                        <List
+                            key={index}
+                            title={title}
+                            deleteList={function () {
+                                deleteList(index);
+                            }}
+                            key={index}
+                            text={List}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
-
-export default App;
